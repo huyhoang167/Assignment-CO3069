650ab5687c067380bc9dc1e4c9284c9beb3f2475
huyhoang167 <huyhoang1672002@gmail.com>
2023-12-03 14:48:53 +0700

    upload source code

---
+++ oidc/src/db/memory.ts
@@ -0,0 +1,8 @@
+export const accounts = new Map()
+
+accounts.set('ebrahimmfadae', {
+	username: 'ebrahimmfadae',
+	password: 'pass123',
+	email: 'ebrahimmfadae@gmail.com',
+	emailVerified: false,
+})
---
+++ oidc/src/index.ts
@@ -0,0 +1,33 @@
+import Koa from 'koa'
+import render from 'koa-ejs'
+import mount from 'koa-mount'
+import koaStatic from 'koa-static'
+import path from 'path'
+import { configuration } from './configs/configuration'
+import { oidc } from './configs/provider'
+import connectMongodb from './db/mongodb/connection'
+import router from './routes'
+
+const start = async () => {
+	await connectMongodb()
+
+	const app = new Koa()
+	render(app, {
+		cache: false,
+		viewExt: 'ejs',
+		layout: false,
+		root: path.resolve('oidc/src/views'),
+	})
+
+	const provider = oidc(process.env.OIDC_ISSUER as string, configuration)
+
+	app.use(koaStatic(path.resolve('public')))
+	app.use(router(provider).routes())
+	app.use(mount(provider.app))
+
+	app.listen(process.env.PORT, () => {
+		console.log(`oidc-provider listening on port ${process.env.PORT}`)
+	})
+}
+
+void start()
